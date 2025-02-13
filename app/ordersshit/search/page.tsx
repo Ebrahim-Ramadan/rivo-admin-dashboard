c4b25e382716c59a62e2ab4626492afc41603a3c
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-13 11:24:44 +0200

    img input placeholder fix

--- app/ordersshit/search/page.tsx
+++ app/ordersshit/search/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Search } from './Search'
 export const Home = () => {
   return (
-    <div  className="flex flex-col gap-4 w-full">
+    <div  className="flex flex-col gap-4 w-full px-2">
 <div className='flex flex-row justify-between p-2 [&>*]:navbararrow'>
   <a href='/ordersshit'> ← Back to Orders</a>
 </div>
--- components/create-frame.tsx
+++ components/create-frame.tsx
@@ -44,7 +44,7 @@ export function CreateFrame() {
         <form onSubmit={handleSubmit} className="mt-4 space-y-4">
           <div>
             <label className="block text-sm font-medium mb-1">Name</label>
-            <Input name="name" required placeholder="do not forget the extension (eg: .png, .jpg)" />
+            <Input name="name" required  />
           </div>
 
           <div>
@@ -79,7 +79,7 @@ export function CreateFrame() {
 
           <div>
             <label className="block text-sm font-medium mb-1">Images (comma-separated)</label>
-            <Input name="images" required />
+            <Input name="images" required placeholder="do not forget the extension (eg: .png, .jpg)" />
           </div>
 
           <div>
